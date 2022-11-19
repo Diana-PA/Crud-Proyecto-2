b2802d24d0e77ce4828aa88711a6c70911d11be8
Diana Arriagada <113148067+Diana-PA@users.noreply.github.com>
2022-11-19 20:41:05 -0300

    Update y Edit
    Se agrega la función de Update y Editar la información.
    Además se agregó el footer.

--- main.js
+++ main.js
@@ -1,78 +1,89 @@
+// Función Crear
 document.getElementById("formulario").addEventListener("submit", crear)
 
- 
-
 function crear(e){
-
     txtNombreApellido = document.getElementById("txtNombreApellido").value
-
     txtCarrera =  document.getElementById("txtCarrera").value
 
- 
-
     let registro = {
-
         txtNombreApellido,
-
         txtCarrera
-
     }
-
     if(localStorage.getItem("Nombres") === null){
-
         let registros = []
-
         registros.push(registro)
-
         localStorage.setItem("Nombres", JSON.stringify(registros))
-
     }else{
-
         let registros = JSON.parse(localStorage.getItem("Nombres"))
-
         registros.push(registro)
-
         localStorage.setItem("Nombres", JSON.stringify(registros))
-
     }
-
     leer();
-
     document.getElementById("formulario").reset();
-
-    e.preventDefaulst();
-
- 
-
+    e.preventDefault();
 }
 
+// Función Leer
 function leer(){
-
     let registros = JSON.parse(localStorage.getItem("Nombres"));
-
     document.getElementById("tbody").innerHTML = ""
-
     for(let i = 0; i < registros.length; i++){
-
         let txtNombreApellido = registros[i].txtNombreApellido
-
-       let  txtCarrera = registros[i].txtCarrera
-
- 
+        let txtCarrera = registros[i].txtCarrera
+       
 
        document.getElementById("tbody").innerHTML +=
-
        ` <tr>
-
-        <td>${txtNombreApellido} </td>
-
-        <td>${txtCarrera} </td>
-
-       </tr>
-
+       <td>${txtNombreApellido} </td>
+       <td>${txtCarrera} </td>
+       <td> 
+       <button type="button" class="btn btn-outline-success" onclick="editar('${txtNombreApellido}')">Editar</button>
+       <button type="button" class="btn btn-outline-danger">Eliminar</button>
+       </td>
+      </tr>
        `
-
     }
+}
+
+// Función Editar
+function editar(nombre){
+    let registros = JSON.parse(localStorage.getItem("Nombres"));
+    for(let i =0; i< registros.length; i++){
+        if(registros[i].txtNombreApellido === nombre){
+            document.getElementById("body").innerHTML =
+             ` 
+             <form id="formulario">
+             <div class="row">
+                 <div class="col-6 col-md-4">
+                     <label>Nombre y Apellido</label>
+                 </div>
+                 <div class="col-6 col-md-4">
+                     <label>Carrera</label>
+                 </div>
+                 <div class="col-6 col-md-4">
+                 <button type="submit" class="addTaskButton" onclick="actualizar('${i}')">Editar</button>
+                 </div>
+                 <div class="col-6 col-md-4">
+                     <input type="text" class="form-control" id="nuevoNombre" aria-describedby="button-addon2" placeholder="${registros[i].txtNombreApellido}">
+                 </div>
+                 <div class="col-6 col-md-4">
+                     <input type="text" class="form-control" id="nuevaCarrera" aria-describedby="button-addon2" placeholder="${registros[i].txtCarrera}">
+                 </div>
+                 <div class="col-6 col-md-4">
+                 </div>
+             </div>
+             </form>
+            `
+        }
+    }
+}
+
+// Funcion Actualizar
+function actualizar(i){
+    let registros = JSON.parse(localStorage.getItem("Nombres"));
+    registros[i].txtNombreApellido = document.getElementById("nuevoNombre").value
+    registros[i].txtCarrera = document.getElementById("nuevaCarrera").value
+    localStorage.setItem("Nombres", JSON.stringify(registros));
 
 }
 
